caf1eb8dd8a34fefa697188e460b8d94724eea9f
mesfinchalnew-creator <mesfinchalnew@gmail.com>
2026-05-11 02:35:14 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///exam_data.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-# Student model with score
+# Student model
 class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True)
@@ -27,17 +27,14 @@ class Question(db.Model):
     option_d = db.Column(db.String(200))
     correct_answer = db.Column(db.String(1))
 
-# ዳታቤዙን ለማጽዳትና በአዲስ ለመፍጠር
 @app.route('/init_db')
 def init_db():
-    db.drop_all() # የድሮውን ስህተት ያለበትን ዳታቤዝ ያጠፋዋል
-    db.create_all() # አዲሱን score ያለበትን ይፈጥራል
+    db.drop_all() 
+    db.create_all() 
     
-    # ተማሪዎችን መጨመሪያ
     for name in ['abdi', 'bezaye', 'mesfin', 'chere', 'solomon']:
         db.session.add(Student(username=name, password='123'))
     
-    # 6 ጥያቄዎች
     qs = [
         Question(text="OSI model layer for routing?", option_a="Physical", option_b="Network", option_c="Transport", option_d="Data Link", correct_answer="B"),
         Question(text="Port for HTTP?", option_a="21", option_b="25", option_c="80", option_d="443", correct_answer="C"),
@@ -71,15 +68,27 @@ def exam():
     if 'user' not in session: return redirect(url_for('index'))
     return render_template('exam.html', questions=Question.query.all())
 
+# --- እዚህ ጋር ነው ውጤቱ እንዲሰላ የተደረገው ---
 @app.route('/submit_exam', methods=['POST'])
 def submit_exam():
     if 'user' not in session: return redirect(url_for('index'))
     
+    questions = Question.query.all()
+    score = 0
+    
+    # እያንዳንዱን የተማሪ መልስ ከዳታቤዙ ጋር ማወዳደር
+    for q in questions:
+        user_answer = request.form.get(f'q{q.id}')
+        if user_answer == q.correct_answer:
+            score += 1
+    
     user_rec = Student.query.filter_by(username=session['user']).first()
     if user_rec:
-        user_rec.score = 4 # ውጤቱን 4 አደረግነው
+        user_rec.score = score
         db.session.commit()
-    return render_template('success.html')
+    
+    # ውጤቱን ወደ ገጹ መላክ
+    return render_template('success.html', score=score, total=len(questions))
 
 @app.route('/admin')
 def admin_panel():
